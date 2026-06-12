ea620b2f235d90c728f267763c972ccbfdcc24b9
qin9125 <32502941+qin9125@users.noreply.github.com>
2026-06-13 00:05:34 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+sudo systemctl stop x-ui || true
+sudo rm -rf x-ui/ /usr/local/x-ui/ /usr/bin/x-ui
+tar zxvf x-ui-linux-amd64.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+sudo cp x-ui/x-ui.sh /usr/bin/x-ui
+sudo mv x-ui/ /usr/local/
+sudo systemctl start x-ui
+sudo rm -f x-ui-linux-amd64.tar.gz
+set +x
