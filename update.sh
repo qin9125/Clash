f1ac24c861a6fb1f634ed383ba372cb21bb177f2
qin9125 <32502941+qin9125@users.noreply.github.com>
2026-09-09 19:46:49 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ set -ex
 sudo systemctl stop x-ui || true
 sudo rm -rf x-ui/ /usr/local/x-ui/ /usr/bin/x-ui
 tar zxvf x-ui-linux-amd64.tar.gz
-chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/bin/mtg-linux-* x-ui/x-ui.sh
 sudo cp x-ui/x-ui.sh /usr/bin/x-ui
 sudo mv x-ui/ /usr/local/
 sudo systemctl start x-ui
